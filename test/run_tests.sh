d3742813726a8e9a3674a7e33b1768681760980e
Terrence Ryan <tpryan@google.com>
2019-12-18 16:12:02 -0800

    Update run_tests.sh

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+sleep 3
 /root/.composer/vendor/phpunit/phpunit/phpunit /var/www/html/DevDefaultURLTest.php 
 /root/.composer/vendor/phpunit/phpunit/phpunit /var/www/html/DevBlogURLTest.php 
